7635736b116b6d4184e0b23cf6247d784bb62bd3
monterourena <davidmonterourena@gmail.com>
2023-03-07 11:53:41 -0600

    core functionality added

--- main.js
+++ main.js
@@ -10,7 +10,27 @@ class Record {
   }
 }
 
-// Listeners and elements
+// Global variables
+let currentRecord, savedRecords;
+
+// General Functions
+function NewToast(Message){
+  Toastify({
+    text: Message,
+    duration: 1500,
+    close: false,
+    gravity: "bottom", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(96.53deg, #6166DE 20.58%, #5CAFCE 96.44%)",
+    }
+  }).showToast();
+}
+
+// LISTENERS AND DOM ELEMENTS
+
+// --> General Elements
 const savedRecordsContainer = document.querySelector("#RecordsContainer");
 
 const CalculateForm = document.querySelector("#CalculateButton");
@@ -21,6 +41,7 @@ SaveButton.addEventListener("click", SaveRecord);
 
 const result = document.getElementById("InvestmentResultOutput");
 
+// --> Slider
 const SliderButtonA = document.querySelector("#SliderButton__Beginning");
 const SliderButtonB = document.querySelector("#SliderButton__End");
 
@@ -36,18 +57,38 @@ SliderButtonB.addEventListener("click", function () {
   Total();
 });
 
-// Global variables
-let currentRecord, savedRecords;
+// --> Filters
+const SetFilterButton = document.querySelector("#SetFilterButton")
+SetFilterButton.addEventListener("click", FilteringRecords)
 
-// Get session
+const ClearFilterButton = document.querySelector("#ClearFilterButton")
+ClearFilterButton.addEventListener("click", function(){
+  LoadRecords(savedRecords)
+  NewToast("Filters cleared")
+})
+
+// SESSION AND LOCAL STORAGE
+const ClearRecordsButton = document.querySelector("#ClearRecordsButton")
+ClearRecordsButton.addEventListener("click", ClearStorage)
+
 if (localStorage.getItem("savedRecords") === null) {
   savedRecords = new Array();
 } else {
   savedRecords = JSON.parse(localStorage.getItem("savedRecords"));
   console.log("Session restored");
   LoadRecords(savedRecords);
+  NewToast("Session restored")
 }
 
+function ClearStorage(){
+  savedRecords = []
+  localStorage.setItem("savedRecords", JSON.stringify(savedRecords));
+  LoadRecords(savedRecords)
+  NewToast("Records cleared")
+}
+
+
+// DOM MANIPULATION FUNCTIONS
 // Get input values
 
 function getPeriod() {
@@ -59,36 +100,48 @@ function getValue(id) {
   return parseFloat(document.getElementById(id).value);
 }
 
-// load stored records
+// Records manipulation
 
-function removeAllChildNodes(parent) {
+function RemoveAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
 }
 
 function LoadRecords(records) {
+
+  console.log("Loading these records:", records)
   //Agregar aquí que se haga display de un card bien bonito si no hay resultados
-  removeAllChildNodes(savedRecordsContainer);
+  RemoveAllChildNodes(savedRecordsContainer);
   for (const record of records) {
     const element = document.createElement("div");
     element.classList.add("Record__Card");
-    element.innerHTML = `<p class="Record__Amount">${record.total}</p>
+    element.innerHTML = `<p class="Record__Amount">${
+      "$ " + record.total.toFixed(2)
+    }</p>
     <div class="Record__Information_Grid">
       <div class="Record__Details">
-        <p class="DetailValue" id="DetailValue1">${record.base}</p>
+        <p class="DetailValue" id="DetailValue1">${
+          "$ " + record.base.toFixed(1)
+        }</p>
         <p class="DetailDescription" id="DetailDescription1">Initial Investment</p>
       </div>
       <div class="Record__Details">
-        <p class="DetailValue" id="DetailValue2">${record.interest}</p>
+        <p class="DetailValue" id="DetailValue2">${
+          record.interest.toFixed(1) + " %"
+        }</p>
         <p class="DetailDescription" id="DetailDescription2">Annual interest rate</p>
       </div>
       <div class="Record__Details">
-        <p class="DetailValue" id="DetailValue3">${record.anual_add}</p>
+        <p class="DetailValue" id="DetailValue3">${
+          "$ " + record.anual_add.toFixed(1)
+        }</p>
         <p class="DetailDescription" id="DetailDescription3">Annual increment</p>
       </div>
       <div class="Record__Details">
-        <p class="DetailValue" id="DetailValue4">${record.years}</p>
+        <p class="DetailValue" id="DetailValue4">${
+          record.years.toFixed(0) + " years"
+        }</p>
         <p class="DetailDescription" id="DetailDescription4">Investment term</p>
       </div>
     </div>`;
@@ -96,7 +149,7 @@ function LoadRecords(records) {
   }
 }
 
-// Compount interest calculation
+// MAIN CALCULATIONS
 
 function Total() {
   let period = getPeriod();
@@ -129,24 +182,22 @@ function Total() {
     result.textContent = "Missing inputs";
     console.log("Missing inputs");
   } else {
-    result.textContent = record.total.toFixed(2);
+    result.textContent = "$ "+record.total.toFixed(2);
     currentRecord = record;
   }
 }
 
 //Función para listar cálculos
-function FilteringRecords(filter) {
+function FilteringRecords() {
   let tempRecords = savedRecords;
 
-  if (filter) {
-    filterValue = parseFloat(
-      prompt("Ingrese el número mínimo de años de inversión")
-    ); //Cambiar esto por el valor que tiene el filtro
-    tempRecords = records.filter(function (record) {
-      return record.years >= filterValue;
-    });
-  }
+  filterValue = getValue("FilterInput")
+  tempRecords = tempRecords.filter(function (record) {
+    return record.years >= filterValue;
+  });
+
   LoadRecords(tempRecords);
+  NewToast("Filter applied")
 }
 
 //AGREGAR FUNCION QUE ME QUITE LOS FILTROS
@@ -154,12 +205,13 @@ function FilteringRecords(filter) {
 function SaveRecord() {
   let lastRecord = currentRecord;
   if (lastRecord == undefined) {
-    //cambiar por un toast
+    NewToast("Invalid record")
     console.log("Invalid calculation");
   } else if (!isNaN(lastRecord.total)) {
     savedRecords.push(lastRecord);
     console.log("Record saved");
     localStorage.setItem("savedRecords", JSON.stringify(savedRecords));
     LoadRecords(savedRecords);
+    NewToast("Record saved")
   }
 }
